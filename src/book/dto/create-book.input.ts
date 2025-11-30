1fb7a639ec2f7b97c178b9fedb3a0ffe72fe82ea
Bibi <irin.m@kkumail.com>
2025-12-01 00:26:50 +0700

    feat: validate dto

--- src/book/dto/create-book.input.ts
+++ src/book/dto/create-book.input.ts
@@ -1,13 +1,27 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 
 @InputType()
 export class CreateBookInput {
   @Field()
+  @IsString()
+  @IsNotEmpty({ message: 'title ห้ามว่าง' })
   title: string;
 
   @Field()
+  @IsString()
+  @IsNotEmpty({ message: 'author ห้ามว่าง' })
   author: string;
 
-  @Field(() => Int)
+  @Field(() => Int, { nullable: true })
+  @IsOptional()
+  @IsInt({ message: 'publishedYear ต้องเป็นจำนวนเต็ม (Int)' })
+  @Min(0, { message: 'publishedYear ต้องมากกว่าหรือเท่ากับ 0' })
   publishedYear?: number;
 }
--- src/book/dto/update-book.input.ts
+++ src/book/dto/update-book.input.ts
@@ -1,7 +1,12 @@
-import { InputType, Field, Int } from '@nestjs/graphql';
+import { InputType, Field, ID, PartialType } from '@nestjs/graphql';
+import { IsMongoId, IsNotEmpty } from 'class-validator';
+
+import { CreateBookInput } from './create-book.input';
 
 @InputType()
-export class UpdateBookInput {
-  @Field()
+export class UpdateBookInput extends PartialType(CreateBookInput) {
+  @Field(() => ID)
+  @IsMongoId({ message: '_id ต้องเป็น Mongo ObjectId ที่ถูกต้อง' })
+  @IsNotEmpty({ message: '_id ห้ามว่าง' })
   _id: string;
 }
